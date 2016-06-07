b5b322a5a93bca1dc4994a65718955395caf66a3
codeliu6572 <codeliu6572@163.com>
2016-06-07 15:37:49 +0800

    NavClear
    渐变导航条

--- NavClear/ViewController.m
+++ NavClear/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UIScrollViewDelegate,UITableViewDelegate,UITableViewDataSource>
+{
+    UIImageView *barImageView;
+    UITableView *myTableView;
+}
 @end
 
 @implementation ViewController
@@ -17,8 +20,50 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor=[UIColor whiteColor];
+//    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
+//                                                  forBarMetrics:UIBarMetricsDefault];
+//    self.navigationController.navigationBar.shadowImage = [UIImage new];
+    self.navigationController.navigationBar.barTintColor=[UIColor orangeColor];
+    
+    self.title=@"This is my title!";
+    self.navigationController.navigationBar.tintColor = [UIColor blackColor];
+    
+    myTableView=[[UITableView alloc]initWithFrame:self.view.bounds];
+    myTableView.delegate=self;
+    myTableView.dataSource=self;
+    [self.view addSubview:myTableView];
+    UIImageView *imageView=[[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 320, 270)];
+    imageView.image=[UIImage imageNamed:@"1.png"];
+    myTableView.tableHeaderView=imageView;
+    
+    barImageView = self.navigationController.navigationBar.subviews.firstObject;
+    
+}
+#pragma mark - UITableViewDelaget
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 40;
+}
+-(CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (!cell) {
+        cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    cell.textLabel.text=[NSString stringWithFormat:@"%ld",indexPath.row];
+    return cell;
 }
 
+
+-(void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    barImageView.alpha=1-scrollView.contentOffset.y/400;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
